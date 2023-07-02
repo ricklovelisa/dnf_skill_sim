7a648f5fd12460d0f84f30161048a28b00cef1aa
Rick Hunter <ricklovelisa@126.com>
2023-07-03 01:40:35 +0800

    update

--- cdr.py
+++ cdr.py
@@ -1,10 +1,12 @@
+from typing import List
+
 from skill import Skill
 
 
 class CDRInfo:
     lingtong_cd_map = {0.1: 4, 0.15: 3, 0.2: 2}
 
-    def __init__(self, skill_info: Skill,
+    def __init__(self,
                  lingtong_pct: float,
                  red_fuwen: int,
                  blue_fuwen: int,
@@ -12,7 +14,6 @@ class CDRInfo:
                  common_cdrr: float,
                  other_cdr: float,
                  other_cdrr: float):
-        self._skill_info = skill_info
         self._lingtong_pct = lingtong_pct
         self._red_fuwen = red_fuwen
         self._blue_fuwen = blue_fuwen
@@ -21,8 +22,8 @@ class CDRInfo:
         self._other_cdr = other_cdr
         self._other_cdrr = other_cdrr
 
-    def get_final_cd(self, skill_time: int):
-        raw_cd = self._skill_info.cd
+    def get_final_cd(self, skill_info: Skill, skill_time: int):
+        raw_cd = skill_info.cd
 
         final_cdr = self._common_cdr * self._other_cdr
         final_cdrr = self._common_cdrr + self._other_cdrr
@@ -31,4 +32,4 @@ class CDRInfo:
             curr_lingtong_cdrr = skill_time % cycle_times * self._lingtong_pct
             final_cdrr += curr_lingtong_cdrr
         print('final_cdr:', final_cdr, ', final_cdrr:', final_cdrr)
-        return final_cdr / (1+final_cdrr) * raw_cd
+        return final_cdr / (1 + final_cdrr) * raw_cd
--- run.py
+++ run.py
@@ -1,9 +1,16 @@
 from typing import List
 
+from cdr import CDRInfo
 from skill import Skill
 
 
-class Experiment:
+class Sim:
 
-    def run_with_time(self, skill_list:List[Skill], cdr_info:,time:int):
+    def run_with_time(self, skill_list: List[Skill], cdr_info: CDRInfo, time: int):
         # 先根据
+        pass
+
+
+if __name__ == '__main__':
+    sim = Sim()
+    sim.run_with_time()
