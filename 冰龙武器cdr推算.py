ef3760c2670630bf3a9d92f9e786f579c32b6f12
Rick Hunter <ricklovelisa@126.com>
2023-07-14 02:10:03 +0800

    update

--- 冰龙武器cdr推算.py
+++ 冰龙武器cdr推算.py
@@ -1,5 +1,11 @@
+import json
+
 import matplotlib.pyplot as plt
 import numpy as np
+
+from cdr import parse_cdr_info
+from skill import parse_skill
+
 DATA_PATH = 'data'
 
 class Calc:
@@ -10,11 +16,18 @@ class Calc:
         plt.plot(raw_cd, final_cd)
         plt.show()
 
-    def compute_cd_set(self, ):
+    def compute_cd_set(self, set_file_name, ):
         with open(f'{DATA_PATH}/{set_file_name}/skill_info.json', 'r') as f:
             skill_list = []
             for skill_info in json.load(f):
                 skill_list.append(parse_skill(skill_info))
+
+            with open(f'{DATA_PATH}/{set_file_name}/cdr_info.json', 'r') as f:
+                cdr_info_json = json.load(f)
+
+            result = parse_cdr_info(skill_list, cdr_info_json)
+
+
 
 
 if __name__ == '__main__':
