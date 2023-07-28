e0d1ef440af1ae8948747987086c35597d26553b
qiuqiu.qq <qiuqiu.qq@bytedance.com>
2023-07-29 03:29:14 +0800

    update

--- commont_test.py
+++ commont_test.py
@@ -1,4 +1,30 @@
-dic = {'a': {'res_cd': 1, 'cnt': 2}, 'b': {'res_cd': 3, 'cnt': 2}, 'c': {'res_cd': 2, 'cnt': 2}}
+# 定义一个简单的树节点类
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
 
-sorted_skill_status_list = sorted(dic.items(), key=lambda x: x[1]['res_cd'], reverse=True)
-print(sorted_skill_status_list)
+# 定义遍历树的函数
+def traverse_tree(root, path=''):
+    # 如果节点为空，则直接返回
+    if root is None:
+        return []
+    # 将当前节点添加到路径上
+    path += str(root.value)
+    # 如果当前节点是叶子节点，则返回当前路径
+    if root.left is None and root.right is None:
+        return [path]
+    else:
+        # 否则，递归遍历左子树和右子树，并将结果合并
+        return traverse_tree(root.left, path+'->') + traverse_tree(root.right, path+'->')
+
+# 建立一个树来测试
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+# 输出所有路径
+print(traverse_tree(root))
