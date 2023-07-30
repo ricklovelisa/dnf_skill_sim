e6fe4f0ccf391d019a64ca9d30159ed9b08030c1
Rick Hunter <ricklovelisa@126.com>
2023-07-31 00:32:30 +0800

    update

--- skill.py
+++ skill.py
@@ -196,33 +196,49 @@ class SkillActionSet:
 
     def exec_without_updating_skill_status(self, skill_status: SkillStatus):
         # 执行技能组，并返回该技能组的总体past_time, total_damage
-        # past_time = 各个技能的wait_time + action_time - force_reduce_time
+        # past_time = 各个技能的wait_time（也就是res_cd） + 前n-1个柔化time + 最后一个技能的action_time
+        # 构建一个字典，用来存储每个技能在本次执行后，实际剩余的cd
+        # {'炸热': {'past_cd':0.05, 'cnt':1}}
+        past_cd_info = {}
 
         if len(self._skill_set) > 1:
             past_time = 0
+            total_damage = 0
             for i in range(len(self._skill_set)):
                 skill = self._skill_set[i]
 
-                # 如果有下一个柔化技能，则action_time直接使用柔化时间
-                if i + 2 <= len(self._skill_set):
+                # 该技能伤害，并汇总
+                total_damage = total_damage + skill.damage
+
+                # 该技能的剩余cd时间，实际的剩余时间需要减去之前的past_time
+                status = skill_status.get_status_by_name(skill.name)
+                res_cd = status['res_cd'] - past_time
+                past_time = past_time + self._human_reflection + res_cd
+                if i + 2 <= len(self._skill_set):  # 如果有下一个柔化技能，则action_time直接使用柔化时间
                     next_skill = self._skill_set[i + 1]
+                    # 柔化技能可使用时间
                     force_action_time = skill.force_next_skill_time[next_skill.name]
-
-
+                    # 累加整体时间，如果下一个技能的cd时间大于force_action_time，则累加下一个技能的cd时间
+                    past_time = past_time + self._human_reflection + force_action_time
+                else:  # 如果是最后一个技能，则action_time使用该技能的
+                    # 该技能的剩余cd时间
+                    past_time = past_time + self._human_reflection + skill.action_time
+            print('skill_set:', json.dumps(self._skill_set, ensure_ascii=False), 'past_time:', past_time)
+            return past_time, total_damage
         else:
             skill = self._skill_set[0]
             status = skill_status.get_status_by_name(skill.name)
             res_cd = status['res_cd']
-            return res_cd + skill.action_time, skill.damage
+            return self._human_reflection + res_cd + skill.action_time, skill.damage
 
     def exec(self, human_reflection: float, skill_status: SkillStatus):
 
 
 class SkillAction:
 
-    def __init__(self, skill_info: Dict[str, Skill]):
+    def __init__(self, skill_info: Dict[str, Skill], human_reflection: float):
         self._skill_list_with_force_skill_set: List[SkillActionSet] = self._make_force_set(skill_info)
-        self._human_reflection
+        self._human_reflection = human_reflection
 
     def _deep_search_force_skill(self, skill: Skill, skill_info: Dict[str, Skill], path: List = None):
         if path is None:
@@ -248,7 +264,7 @@ class SkillAction:
         for _, skill in skill_info.items():
             result.extend(self._deep_search_force_skill(skill=skill, skill_info=skill_info))
 
-        return [SkillActionSet(x) for x in result]
+        return [SkillActionSet(self._human_reflection, x) for x in result]
 
     def _single_skill_action(self, skill: Skill, ):
         return skill.action_time,
@@ -256,12 +272,7 @@ class SkillAction:
     def exec(self, skill_status: SkillStatus):
         skill_actions = []
         for skill_set in self._skill_list_with_force_skill_set:
-            # 获取技能cd状态 {"res_cd":0.4,"cnt":1}
-            for skill in skill_set:
-                status = skill_status.get_status_by_name(skill_name)
-
-            if skill.force_next_skill_time:
-                force_skill_set = self._make_force_set(start)
+    # 获取技能cd状态 {"res_cd":0.4,"cnt":1}
 
 
 if __name__ == '__main__':
@@ -271,4 +282,4 @@ if __name__ == '__main__':
         for skill_name, skill in json.load(f).items():
             skill_info[skill_name] = Skill(skill_name, skill)
 
-    sa = SkillAction(skill_info)
+    sa = SkillAction(skill_info, 0.1)
