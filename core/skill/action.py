95882f1a05fa550cdf52d1183cbfe02819cdf9ec
qiuqiu.qq <qiuqiu.qq@bytedance.com>
2023-08-11 21:38:37 +0800

    update

--- core/skill/action.py
+++ core/skill/action.py
@@ -3,7 +3,7 @@ import random
 from abc import abstractmethod
 from typing import Dict, Union, List
 
-from core.skill.definition import Skill
+from core.skill.definition import Skill, SkillSet, SkillQueue
 
 
 class SkillStatus:
@@ -64,126 +64,60 @@ class SkillStatus:
         return self._skill_status_map[skill_name]['res_cd']
 
 
-class SkillAction:
+# class SkillAction:
+#
+#     def __init__(self, skill_info: Dict[str, Skill], human_reflection: float, is_op: bool):
+#         self._human_reflection = human_reflection
+#         self._is_op = is_op
+#         self._skill_list_with_force_skill_set: List[SkillSet] = self._make_force_set(skill_info)
+#
+#     def __str__(self):
+#         result = []
+#         for skill_set in self._skill_list_with_force_skill_set:
+#             result.append([x.name for x in skill_set.skills])
+#         return json.dumps({'human_reflection': self._human_reflection, 'is_op': self._is_op,
+#                            'skill_set': result}, ensure_ascii=False)
+#
+#     def __repr__(self):
+#         return self.__str__()
+#
+#
+#
+#     @property
+#     def skill_sets(self):
+#         return self._skill_list_with_force_skill_set
+#
+#     def return_skill_set_with_skill_status(self, skill_status: SkillStatus):
+#         """ 根据实际的skill_status，计算每个技能的past_time，和实际伤害、各个技能的实际秒伤
+#
+#         """
+#
+#         result = []
+#         for skill_set in self._skill_list_with_force_skill_set:
+#             past_time, damage, dps, next_cd = skill_set.compute_past_and_damage(self._is_op, skill_status)
+#             # 获取这个技能组的max(res_cd)作为 res_cd,
+#             max_res_cd = 0
+#             for skill in skill_set.skills:
+#                 max_res_cd = max(skill_status.get_skill_res_cd(skill.name), max_res_cd)
+#             # 获取技能组中每个技能的当前使用次数
+#             curr_cnt = [skill_status.get_skill_cnt(x.name) for x in skill_set.skills]
+#             result.append(
+#                 {'skill_set': skill_set, 'res_cd': max_res_cd, 'past_time': past_time, 'damage': damage, 'dps': dps,
+#                  'curr_cnt': curr_cnt, 'next_cd': next_cd})
+#
+#         return result
 
-    def __init__(self, skill_info: Dict[str, Skill], human_reflection: float, is_op: bool):
-        self._human_reflection = human_reflection
+
+class SkillSetAction:
+
+    def __init__(self, is_op: bool, human_reflection: float, skill_set: SkillSet):
         self._is_op = is_op
-        self._skill_list_with_force_skill_set: List[SkillSet] = self._make_force_set(skill_info)
-
-    def __str__(self):
-        result = []
-        for skill_set in self._skill_list_with_force_skill_set:
-            result.append([x.name for x in skill_set.skills])
-        return json.dumps({'human_reflection': self._human_reflection, 'is_op': self._is_op,
-                           'skill_set': result}, ensure_ascii=False)
-
-    def __repr__(self):
-        return self.__str__()
-
-    def _deep_search_force_skill(self, skill: Skill, skill_info: Dict[str, Skill], path: List = None):
-        if path is None:
-            path = []
-        paths = []
-        current_path = path + [skill]
-        if skill.force_next_skill_time:
-            for next_skill_name, force_time in skill.force_next_skill_time.items():
-                if next_skill_name in skill_info:
-                    next_skill = skill_info[next_skill_name]
-                    if not next_skill.force_next_skill_time:
-                        paths.append(current_path + [next_skill])
-                    else:
-                        for sub_path in self._deep_search_force_skill(skill=next_skill, skill_info=skill_info,
-                                                                      path=current_path):
-                            paths.append(sub_path)
-        else:
-            paths.append(current_path)
-        return paths
-
-    def _make_force_set(self, skill_info: Dict[str, Skill]):
-        result = []
-        # 针对有柔化技能的技能进行遍历，获得柔化技能组
-        for _, skill in skill_info.items():
-            result.extend(self._deep_search_force_skill(skill=skill, skill_info=skill_info))
-
-        other_skills = []
-        for sets in result:
-            if len(sets) >= 2:
-                other_skills.extend(sets[0:-1])
-
-        result.extend([[x] for x in set(other_skills)])
-
-        return [SkillSet.parse(x, self._human_reflection) for x in result]
-
-    @property
-    def skill_sets(self):
-        return self._skill_list_with_force_skill_set
-
-    def return_skill_set_with_skill_status(self, skill_status: SkillStatus):
-        """ 根据实际的skill_status，计算每个技能的past_time，和实际伤害、各个技能的实际秒伤
-
-        """
-
-        result = []
-        for skill_set in self._skill_list_with_force_skill_set:
-            past_time, damage, dps, next_cd = skill_set.compute_past_and_damage(self._is_op, skill_status)
-            # 获取这个技能组的max(res_cd)作为 res_cd,
-            max_res_cd = 0
-            for skill in skill_set.skills:
-                max_res_cd = max(skill_status.get_skill_res_cd(skill.name), max_res_cd)
-            # 获取技能组中每个技能的当前使用次数
-            curr_cnt = [skill_status.get_skill_cnt(x.name) for x in skill_set.skills]
-            result.append(
-                {'skill_set': skill_set, 'res_cd': max_res_cd, 'past_time': past_time, 'damage': damage, 'dps': dps,
-                 'curr_cnt': curr_cnt, 'next_cd': next_cd})
-
-        return result
-
-    def clean(self):
-        for skill_set in self._skill_list_with_force_skill_set:
-            skill_set.clean()
-
-
-class SkillSet:
-    """ 该类用于描述一些职业的所有的可执行技能组合，这些组合包括单个技能，以及多个相互间可柔化技能。
-
-    例如[炸热、无双、不动]或[无为法]
-
-    构建本对象，是为了方便进行针对技能组的数据统计，例如技能组的总伤，总耗时，总dps等
-    """
-
-    def __init__(self, human_reflection: float, skill_set: List[Skill]):
-        self._skill_set = skill_set
         self._human_reflection = human_reflection
-
-    def __str__(self):
-        return json.dumps([x.name for x in self._skill_set], ensure_ascii=False)
-
-    def __repr__(self):
-        return self.__str__()
-
-    @property
-    def skills(self):
-        return self._skill_set
-
-    @property
-    def action_time(self):
-        skill_set_len = len(self._skill_set)
-        if skill_set_len == 1:
-            return self._skill_set[0].action_time
-        else:
-            action_time = 0
-            for i in range(skill_set_len):
-                skill = self._skill_set[i]
-                if i < skill_set_len - 1:
-                    next_skill = self._skill_set[i + 1]
-                    action_time += skill.force_next_skill_time[next_skill.name]
-                else:
-                    action_time += skill.action_time
-            return action_time
+        self._skill_set = skill_set
+        self._queue = None
 
     @abstractmethod
-    def compute_past_and_damage(self, is_op: bool, skill_status: SkillStatus):
+    def compute_past_and_damage(self, skill_status: SkillStatus):
         """ 计算该技能组根据实际skill_status下的past_time、总伤、秒伤的和
 
         :param is_op:
@@ -193,88 +127,98 @@ class SkillSet:
         pass
 
     @abstractmethod
-    def execution(self, time_line: float, skill_status: SkillStatus):
+    def execution(self, skill_queue: SkillQueue, skill_status: SkillStatus):
         """ 执行，更新skill_status中的技能cd和技能数，并返回时间轴
 
         :return:
         """
         pass
 
-    @abstractmethod
-    def clean(self):
-        pass
-
-    @staticmethod
-    def parse(skill_set: List[Skill], human_reflection=0.1):
-        if len(skill_set) == 1:
-            return SingleSkill(skill_set=skill_set, human_reflection=human_reflection)
-        elif len(skill_set) >= 2:
-            return ForcedSkillSet(skill_set=skill_set, human_reflection=human_reflection)
-        else:
-            raise Exception('skill set长度不能为0')
+    @property
+    def queue(self):
+        return self._queue
 
 
-class SingleSkill(SkillSet):
+class SingleSkillAction(SkillSetAction):
     """ 单技能Set，等价于单个技能
     """
 
-    def __init__(self, human_reflection: float, skill_set: List[Skill]):
-        super().__init__(human_reflection, skill_set)
+    def __init__(self, is_op: bool, human_reflection: float, skill_set: SkillSet):
         # 本次技能组整体耗时
-        self._past_time = None
-        # 本次技能组技能释放完后的剩余cd数
-        self._res_cd = None
-        # 本次技能组技能增加技能次数
-        self._skill_cnt = None
+        super().__init__(is_op, human_reflection, skill_set)
+        # self._past_time = None
+        # # 本次技能组技能释放完后的剩余cd数
+        # self._res_cd = None
+        # # 本次技能组技能增加技能次数
+        # self._skill_cnt = None
 
-    def compute_past_and_damage(self, is_op: bool, skill_status: SkillStatus):
-        skill = self._skill_set[0]
+    def compute_past_and_damage(self, skill_status: SkillStatus):
+        skill_queue = []
+        skill_time_line = []
+        res_cd_list = []
+
+        skill = self._skill_set.skills[0]
+        skill_queue.append(skill)
+
         res_cd = skill_status.get_skill_res_cd(skill.name)
         # 释放本次技能组需要的总时间，本技能的剩余cd，人的反应，以及本技能的action_time
-        self._past_time = res_cd + self._human_reflection + skill.action_time
+        past_time = res_cd + self._human_reflection + skill.action_time
+        skill_time_line.append(past_time)
 
         # 记录本次技能的技能次数和技能剩余cd
         skill_cnt = skill_status.get_skill_cnt(skill.name) + 1
-        next_cd = skill.get_final_cd(is_op, skill_cnt)
-        self._res_cd = next_cd - skill.during
-        self._skill_cnt = skill_cnt
+        next_cd = skill.get_final_cd(self._is_op, skill_cnt)
+        res_cd = next_cd - skill.during
+        res_cd_list.append(res_cd)
+        self._queue = SkillQueue(skill_queue=skill_queue, skill_time_line=skill_time_line, res_cd_list=res_cd_list)
 
-        return self._past_time, skill.damage, skill.damage / next_cd, next_cd
+        # return past_time, skill.damage, skill.damage / next_cd, next_cd
 
-    def execution(self, time_line: float, skill_status: SkillStatus):
-        this_skill_name = self._skill_set[0].name
-        # 本次技能组的技能进入cd，res_cd是实际剩余的cd数
-        skill_status.start_cooling_down(this_skill_name, self._res_cd)
+    def execution(self, executed_skill_queue: SkillQueue, skill_status: SkillStatus):
+        # 将本次技能组的queue合并入executed_skill_queue
+        executed_skill_queue.append(self._queue)
 
-        # 本次技能组的技能技能数
-        add_cnt = self._skill_cnt - skill_status.get_skill_cnt(this_skill_name)
-        skill_status.add_skill_cnt(this_skill_name, add_cnt)
+        # 更新skill_status信息
+        for info in self._queue.loop_info:
+            skill_name = info.skill.name
+            skill_res_cd = info.skill_res_cd
+            skill_status.start_cooling_down(skill_name, skill_res_cd)
 
-        # 其他技能进行cd，时间是本次技能的past_time
-        skill_status.cooling_down(self._past_time, [this_skill_name])
+            skill_status.add_skill_cnt(skill_name, 1)
+
+        total_past_time = self._queue.
+        skill_status.cooling_down(self._past_time, )
+
+        # this_skill_name = self._skill_set[0].name
+        # # 本次技能组的技能进入cd，res_cd是实际剩余的cd数
+        # skill_status.start_cooling_down(this_skill_name, self._res_cd)
+        #
+        # # 本次技能组的技能技能数
+        # add_cnt = self._skill_cnt - skill_status.get_skill_cnt(this_skill_name)
+        # skill_status.add_skill_cnt(this_skill_name, add_cnt)
+        #
+        # # 其他技能进行cd，时间是本次技能的past_time
+        # skill_status.cooling_down(self._past_time, [this_skill_name])
 
         # 最后返回整体的time_line
-        return time_line + self._past_time
+        # return time_line + self._past_time
 
-    def clean(self):
-        self._past_time = None
-        self._res_cd = None
-        self._skill_cnt = None
+    def queue(self):
+        return self._queue
 
 
-class ForcedSkillSet(SkillSet):
+class ForcedSkillSetAction(SkillSetAction):
     """ 多个有柔化技能的组合，在进行相关计算的时候
 
     """
 
-    def __init__(self, human_reflection: float, skill_set: List[Skill]):
-        super().__init__(human_reflection, skill_set)
-        # {skill.name:{'res_cd':0,'add_cnt':1}}
+    def __init__(self, is_op: bool, human_reflection: float, skill_set: SkillSet):
+        super().__init__(is_op, human_reflection, skill_set)
         self._skill_status = {}
         # 本次技能组整体耗时
         self._past_time = 0
 
-    def compute_past_and_damage(self, is_op: bool, skill_status: SkillStatus):
+    def compute_past_and_damage(self, skill_status: SkillStatus):
         # 执行技能组，并返回该技能组的总体past_time, total_damage
         # past_time = 各个技能的wait_time（也就是res_cd） + 前n-1个柔化time + 最后一个技能的action_time
         total_damage = 0
@@ -310,7 +254,7 @@ class ForcedSkillSet(SkillSet):
             max_next_cd = max(max_next_cd, next_cd)
         return self._past_time, total_damage, total_dps, max_next_cd
 
-    def execution(self, time_line: float, skill_status: SkillStatus):
+    def execution(self, skill_queue: SkillQueue, skill_status: SkillStatus):
         # 根据每个技能情况，分别设置技能的res_cd
         for skill_name, status_info in self._skill_status.items():
             # 用完整的past_time - start_cd_time就是该技能走过的时间，然后再用下一次的实际cd-该值就是实际剩余cd
@@ -328,8 +272,7 @@ class ForcedSkillSet(SkillSet):
         skill_status.cooling_down(self._past_time, [x.name for x in self._skill_set])
 
         # 最后返回整体的time_line
-        return time_line + self._past_time
+        # return time_line + self._past_time
 
-    def clean(self):
-        self._skill_status = {}
-        self._past_time = 0
+    def queue(self):
+        return self._queue
--- core/skill/definition.py
+++ core/skill/definition.py
@@ -1,5 +1,6 @@
 import copy
-from typing import Dict
+import json
+from typing import Dict, List
 
 from core.skill.cdr import CDRInfo, make_cdr_info
 
@@ -68,9 +69,6 @@ class Skill:
         return f'name: {self.name}, level: {self.level}, cd: {self.cd}, cast_time: {self.cast_time}, ' \
                f'during: {self.during}, force_next_skill_time: {self.force_next_skill_time}, damage: {self.damage}'
 
-    def __str__(self):
-        return self.detail
-
     def add_cdr_info(self, cdr_info, fuwen_info):
         self._cdr_info = make_cdr_info(self.name, self.level, cdr_info, fuwen_info)
 
@@ -120,3 +118,191 @@ class Skill:
                 new_skill_info[key] = value
 
         return Skill(skill_name, new_skill_info)
+
+
+class SkillSet:
+    """ 该类用于描述一些职业的所有的可执行技能组合，这些组合包括单个技能，以及多个相互间可柔化技能。
+
+    例如[炸热、无双、不动]或[无为法]
+
+    构建本对象，是为了方便进行针对技能组的数据统计，例如技能组的默认总伤，总耗时，总dps等
+    """
+
+    def __init__(self, skills: List[Skill]):
+        self._skills = skills
+
+    def __str__(self):
+        return json.dumps([x.name for x in self._skills], ensure_ascii=False)
+
+    def __repr__(self):
+        return self.__str__()
+
+    @property
+    def skills(self):
+        return self._skills
+
+    @property
+    def action_time(self):
+        """ 该技能组合的最短action_time
+        只考虑技能之间的柔化时间和最后一个技能的action时间
+        :return:
+        """
+        skill_set_len = len(self._skills)
+        if skill_set_len == 1:
+            return self._skills[0].action_time
+        else:
+            action_time = 0
+            for i in range(skill_set_len):
+                skill = self._skills[i]
+                if i < skill_set_len - 1:  # 如果不是最后一个技能，则累加下一个技能的柔化时间
+                    next_skill = self._skills[i + 1]
+                    action_time += skill.force_next_skill_time[next_skill.name]
+                else:  # 如果是最后一个技能，则加上最后一个技能的action时间
+                    action_time += skill.action_time
+            return action_time
+
+    # @staticmethod
+    # def parse(skill_set: List[Skill], human_reflection=0.1):
+    #     if len(skill_set) == 1:
+    #         return SingleSkill(skill_set=skill_set, human_reflection=human_reflection)
+    #     elif len(skill_set) >= 2:
+    #         return ForcedSkillSet(skill_set=skill_set, human_reflection=human_reflection)
+    #     else:
+    #         raise Exception('skill set长度不能为0')
+
+
+class SkillQueue:
+    class LoopBody:
+        def __init__(self, skill: Skill, skill_past_time: float, skill_res_cd: float):
+            self.skill = skill
+            self.skill_past_time = skill_past_time
+            self.skill_res_cd = skill_res_cd
+
+    def __init__(self, ):
+        self._queue = []
+        self._skill_past_time_line = []
+        self._res_cd_list = []
+
+    def __str__(self):
+        return json.dumps(self.skill_name_list, ensure_ascii=False)
+
+    def __repr__(self):
+        return self.__str__()
+
+    @property
+    def skill_name_list(self):
+        return [x.name for x in self._queue]
+
+    @property
+    def total_past_time(self):
+        return sum(self._skill_past_time_line)
+
+    @property
+    def loop_info(self):
+        result = []
+        for skill, skill_past_line, skill_res_cd in zip(self._queue, self._skill_past_time_line, self._res_cd_list):
+            result.append(self.LoopBody(skill, skill_past_line, skill_res_cd))
+
+        return result
+
+    def add_skill(self, skill: Skill):
+        self._queue.append(skill)
+
+    def add_skill_past_time(self, past_time: float):
+        self._skill_past_time_line.append(past_time)
+
+    def add_skill_res_cd(self, res_cd: float):
+        self._res_cd_list.append(res_cd)
+
+    def plot(self, total_time):
+        pass
+
+    def compute_total_damage(self, total_time):
+        pass
+
+    def compute_damage_by_skill(self, total_time):
+        pass
+
+    def append(self, obj):
+        if isinstance(obj, SkillQueue):
+            self._queue.extend(obj._queue)
+            self._skill_past_time_line.extend(obj._skill_past_time_line)
+            self._res_cd_list.extend(obj._res_cd_list)
+        else:
+            raise TypeError('obj不是SkillQueue类')
+
+        # def __str__(self):
+    #     return json.dumps(self.skill_name_list, ensure_ascii=False)
+    #
+    # def __repr__(self):
+    #     return self.__str__()
+
+    # def compute_total_damage(self):
+    #     damage_by_skill = self.compute_damage_by_skill()
+    #
+    #     total_damage = 0
+    #     for item in damage_by_skill:
+    #         total_damage += item['damage']
+    #     return total_damage
+    #
+    # def compute_damage_by_skill(self):
+    #     damage_dict = {}
+    #     for skill in self._queue:
+    #         if skill.name in damage_dict:
+    #             damage_dict[skill.name]['times'] += 1
+    #         else:
+    #             damage_dict[skill.name] = {'times': 1}
+    #
+    #     for skill in self._queue:
+    #         damage = skill.get_final_damage(self._total_time, damage_dict[skill.name]['times'])
+    #         damage_dict[skill.name]['damage'] = damage
+    #
+    #     damage_list = [{'name': k, 'times': v['times'], 'damage': v['damage']} for k, v in damage_dict.items()]
+    #     damage_list = sorted(damage_list, key=lambda x: x['damage'], reverse=True)
+    #
+    #     return damage_list
+    #
+    # @property
+    # def list(self):
+    #     return self._queue
+    #
+    # @property
+    # def skill_name_list(self):
+    #     return [x.name for x in self._queue]
+
+
+def deep_search_force_skill(skill: Skill, skill_info: Dict[str, Skill], path: List = None):
+    if path is None:
+        path = []
+    paths = []
+    current_path = path + [skill]
+    if skill.force_next_skill_time:
+        for next_skill_name, force_time in skill.force_next_skill_time.items():
+            if next_skill_name in skill_info:
+                next_skill = skill_info[next_skill_name]
+                if not next_skill.force_next_skill_time:
+                    paths.append(current_path + [next_skill])
+                else:
+                    for sub_path in deep_search_force_skill(skill=next_skill, skill_info=skill_info,
+                                                            path=current_path):
+                        paths.append(sub_path)
+    else:
+        paths.append(current_path)
+    return paths
+
+
+def make_force_set(skill_info: Dict[str, Skill]):
+    result = []
+    # 针对有柔化技能的技能进行遍历，获得柔化技能组
+    for _, skill in skill_info.items():
+        result.extend(deep_search_force_skill(skill=skill, skill_info=skill_info))
+
+    # 柔化技能组除最后一个技能外，其他技能都添加到set中
+    other_skills = []
+    for sets in result:
+        if len(sets) >= 2:
+            other_skills.extend(sets[0:-1])
+
+    result.extend([[x] for x in set(other_skills)])
+
+    return [SkillSet(x) for x in result]
